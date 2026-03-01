89221f08827f1b49d7de4914e857bd0af3590b4f
ofarino <ofarino@ufl.edu>
2026-03-01 17:58:04 -0500

    reworte lru logic

--- src/lru.py
+++ src/lru.py
@@ -1,23 +1,18 @@
+from collections import OrderedDict
 # Least Recently Used (LRU) Cache implementation 
 def lru_cache(cache_size, requests):
-    cache = {}
+    cache = OrderedDict()  # use an ordered dictionary to maintain the order of items in the cache
     cache_misses = 0
     for request in requests:
         if request in cache:
-            # update the last used time
-            cache[request] = 0
+            # if the request is already in the cache, move it to the end to show that it was recently used
+            cache.move_to_end(request)
         else:
+            # if the request is not in the cache, it's a cache miss
             cache_misses += 1
-            if len(cache) < cache_size:
-                cache[request] = 0
-            else:
-                # find the least recently used item and remove it
-                lru_item = min(cache, key=cache.get)
-                del cache[lru_item]
-                cache[request] = 0
-        # update the last used time for all items in the cache
-        for key in cache:
-            if key != request:
-                cache[key] += 1
-    # return the number of cache misses
+            # if the cache is full, remove the least recently used item (the first item in the ordered dictionary)
+            if len(cache) >= cache_size:
+                cache.popitem(last=False)
+            # add the new request to the cache
+            cache[request] = True
     return cache_misses
