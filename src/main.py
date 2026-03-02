97d6041a0ae750c2ffdf8e41e0ed7e1ccaefddf5
bcsanadi <bcsanadi01@gmail.com>
2026-03-02 10:57:54 -0500

    Added the optff logic

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import lru
 import fifo
+import optff
 
 #read the input file
 def read_input_file(file_path):
@@ -16,7 +17,9 @@ if __name__ == "__main__":
     cache_size, num_requests, requests = read_input_file("./data/example.in")
     lru_cache_misses = lru.lru_cache(cache_size, requests)
     fifo_cache_misses = fifo.fifo_cache(cache_size, requests)
+    optff_cache_misses = optff.optff_cache(cache_size, requests)
     # output the number of cache misses to example.out
     with open("./tests/example.out", 'w') as file:
         file.write('FIFO : ' + str(fifo_cache_misses) + '\n')
         file.write('LRU : ' + str(lru_cache_misses) + '\n')
+        file.write('OPTFF : ' + str(optff_cache_misses) + '\n')
---
+++ src/optff.py
@@ -0,0 +1,26 @@
+# Among items currently in the cache, evict the one whose next request occurs farthest in the future (or never occurs again).
+def optff_cache(cache_size, requests):
+    cache = []
+    cache_misses = 0
+
+    for i, request in enumerate(requests):
+        if request not in cache:
+            cache_misses += 1
+            if len(cache) < cache_size:
+                cache.append(request)
+            else:
+                # Find the item in the cache that is requested farthest in the future
+                farthest_index = -1
+                farthest_request = None
+                for item in cache:
+                    try:
+                        index = requests.index(item, i + 1)
+                    except ValueError:
+                        index = float('inf')  # Item is not requested again
+                    if index > farthest_index:
+                        farthest_index = index
+                        farthest_request = item
+                cache.remove(farthest_request)  # Evict the item that is requested farthest in the future
+                cache.append(request)
+
+    return cache_misses
